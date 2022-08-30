d2f5e5e409dce5d591fdd41c271c68b4b637967c
diegoalvarez17 <diegoalvarez1237@hotmail.com>
2022-08-30 16:35:54 -0600

    LSR
    remaining work

--- lsr.py
+++ lsr.py
@@ -1,31 +1,45 @@
 
+import numpy
+import json
 S = set() 
 G = [] # adjacency matrix
-  
-# give input matrix
-for i in range(4): 
-    listo = [0, 0, 0, 0]
-      
-    for j in range(4):
-        listo[j] = int(input("give input"))
-    G.append(listo)
+
+# Positions of nodes in matrix
+dicLetters = {
+    "A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7, "I": 8,
+    "J": 9, "K": 10, "L": 11, "M": 12, "N": 13, "O": 14, "P": 15, "Q": 16, "R": 17,
+    "S": 18, "T": 19, "U": 20, "V": 21, "W": 22, "X": 23, "Y": 24, "Z": 25
+  }
+
+totalNodes = 0
+with open("topologia.txt") as f:
+    json_data = json.load(f)
+    sizeMatrix = len(json_data['config'])
+
+    for i in json_data['config']:
+        lst = [999] * sizeMatrix
+        for neighbor in json_data['config'][i]:
+            lst[dicLetters[neighbor]] = 1
+        G.append(lst)    
+print(G)
+# exit()
       
 source = int(input("give source")) 
 destination = int(input("give destination")) 
 Q = [] # empty queue
   
-for i in range(4):
+for i in range(sizeMatrix):
     Q.append(i)
       
-d =[0, 0, 0, 0] # initialize d values
-pi =[0, 0, 0, 0] # initialize pi values
+d = [0] * sizeMatrix # initialize d values
+pi =[0] * sizeMatrix # initialize pi values
   
-for i in range(4):
+for i in range(sizeMatrix):
     if(i == source):
         d[i] = 0
     else:
         d[i] = 999
-for i in range(4):
+for i in range(sizeMatrix):
     pi[i] = 9000
 S.add(source)
   
@@ -47,7 +61,7 @@ while (len(Q) != 0):
     Q.remove(u) # removed the minimum vertex
     S.add(u)
     adj = []
-    for y in range(4):
+    for y in range(sizeMatrix):
           
         # find adjacent vertices to minimum vertex
         if(y != u and G[u][y]!= 999):     
