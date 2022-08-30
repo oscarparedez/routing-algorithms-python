6235ac024f064e3cc0587efff0d81ad02e45b4bd
diegoalvarez17 <diegoalvarez1237@hotmail.com>
2022-08-30 15:40:22 -0600

    Client and flooding
    Co-Authored-By: GuidoPadilla <46847917+GuidoPadilla@users.noreply.github.com>
    Co-Authored-By: Oscar Paredez <42584081+oscarparedez@users.noreply.github.com>

--- client.py
+++ client.py
@@ -5,24 +5,36 @@ import slixmpp
 import xmpp
 from slixmpp.xmlstream import ET
 from slixmpp.exceptions import IqError, IqTimeout
+from flooding import *
+from aioconsole import *
+
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 class XMPPChat(slixmpp.ClientXMPP):
 
-    def __init__(self, jid, password):
+    def __init__(self, jid, password, algorithmToUse):
         slixmpp.ClientXMPP.__init__(self, jid, password)
         self.user = jid
+        self.algorithmToUse = algorithmToUse
         self.add_event_handler("session_start", self.start)
-    
-    async def start(self, event):
+        self.add_event_handler("message", self.messageNotifications)
 
-        print("Welcome! ", self.user)
+    async def messageNotifications(self, event):
+        message = event['body']
+        message_from = event['from']
+        await aprint("%s says: %s" % (message_from, message) )
+
+    async def start(self, event):
+        self.send_presence('chat', 'a')
+
+        await aprint("Welcome! ", self.user)
         await self.get_roster()
 
-        print("1. Send Private Message \n2. Log Out")
-        option = input("")
+        await aprint("1. Send Private Message \n2. Log Out")
+        option = await ainput("")
         if option == "1":
-            userDestiny = input("Type email to send message: ")
-            message = input("Message: ")
+            userDestiny = await ainput("Type email to send message: ")
+            message = await ainput("Message: ")
 
             protocolMessage = ""
             # Ejecutar algoritmo
@@ -30,10 +42,22 @@ class XMPPChat(slixmpp.ClientXMPP):
             distance = ""
             listNodes = ""
 
-            protocolMessage += self.user + ';' + userDestiny + ';' + jumps + ';' + distance + ';' + listNodes + message
-            
-            self.send_message(mto=userDestiny, mbody=protocolMessage, mtype='chat')
-            await asyncio.sleep(0.5)
+            if (self.algorithmToUse == "1"):
+                destiny = flooding()
+            elif (self.algorithmToUse == "2"):
+                destiny = flooding()
+            elif (self.algorithmToUse == "3"):
+                destiny = flooding()
+            else:
+                # Default algorithm
+                destiny = flooding()
+
+            for i in destiny:
+                await aprint('aaaa', i)
+                protocolMessage += self.user + ';' + userDestiny + ';' + jumps + ';' + distance + ';' + listNodes + ';' + message
+                
+                # self.send_message(mto=userDestiny, mbody=protocolMessage, mtype='chat')
+                await asyncio.sleep(0.5)
 
             await self.start(event)
         elif (option == "2"):
@@ -41,7 +65,7 @@ class XMPPChat(slixmpp.ClientXMPP):
             self.disconnect()
             await self.start(event)
         else: 
-            print("Try another option!")
+            await aprint("Try another option!")
             await self.start(event)
 
 
@@ -49,10 +73,13 @@ print("--- Log In ---")
 email = input("Email: ")
 password = input("Password: ")
 
+print("1. LSR \n2. Floding \n3. DVR")
+algorithmToUse = input("Please type algorithm to use: ")
+
 email = "alvarez@alumchat.fun"
 password = "swais"
 
-chat = XMPPChat(email, password) 
+chat = XMPPChat(email, password, algorithmToUse) 
 chat.register_plugin('xep_0030') # Service Discovery
 chat.register_plugin('xep_0199') # XMPP Ping
 chat.register_plugin("xep_0085")
